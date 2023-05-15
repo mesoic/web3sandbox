873235cb6bcdcc4f2437a31b6279a1511a34aff2
mesoic <mesoic@protonmail.com>
2023-05-15 17:27:08 -0500

    add go msgs

--- go/msg/msg.go
+++ go/msg/msg.go
@@ -4,7 +4,34 @@ import "fmt"
 
 type Msg struct{}
 
-func (*Msg) Warning(msg string) {
+func (*Msg) Verified(msg string) {
+	fmt.Printf("  verified: %s\n", msg)
+}
 
-	fmt.Printf("%s", msg)
+func (*Msg) Warning(msg string) {
+	fmt.Printf("  warning: %s\n", msg)
+}
+
+func (*Msg) Error(msg string) {
+	fmt.Printf("  error: %s\n", msg)
+}
+
+func (*Msg) Critical(msg string) {
+	fmt.Printf("  critical: %s\n", msg)
+}
+
+func (*Msg) Info(msg string) {
+	fmt.Printf("  info: %s\n", msg)
+}
+
+func (*Msg) Message(msg string) {
+	fmt.Printf("  %s\n", msg)
+}
+
+func (*Msg) Log(msg string) {
+	fmt.Printf("  > %s\n", msg)
+}
+
+func (*Msg) Newline(msg string) {
+	fmt.Printf("\n")
 }
